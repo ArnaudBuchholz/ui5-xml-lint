f6b2b86cb5bd92795b2d442586238910012db004
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-21 21:39:49 -0400

    Test download

--- src/download.spec.ts
+++ src/download.spec.ts
@@ -1,9 +1,38 @@
 import { download } from './download'
+import nock from 'nock'
 
 describe('download', () => {
-  it('generates HTTP gets', async () => {
-    const response = await download('https://sapui5.hana.ondemand.com/1.99.0/discovery/all_libs')
+  beforeEach(() => {
+    nock('https://openui5.hana.ondemand.com')
+      .get('/1.99.0/discovery/all_libs')
+      .reply(200, {
+        all_libs: [{
+          entry: 'sap/f'
+        }, {
+          entry: 'sap/m'
+        }]
+      })
+
+    nock('http://openui5.hana.ondemand.com')
+      .get('/1.98.0/discovery/all_libs')
+      .reply(200, {
+        all_libs: [{
+          entry: 'sap/f'
+        }, {
+          entry: 'sap/m'
+        }]
+      })
+  })
+
+  it('handles HTTPS', async () => {
+    const response = await download('https://openui5.hana.ondemand.com/1.99.0/discovery/all_libs')
     const allLibs = JSON.parse(response)
-    expect(allLibs.all_libs.length).toStrictEqual(24)
+    expect(allLibs.all_libs.length).toStrictEqual(2)
+  })
+
+  it('handles HTTP', async () => {
+    const response = await download('http://openui5.hana.ondemand.com/1.98.0/discovery/all_libs')
+    const allLibs = JSON.parse(response)
+    expect(allLibs.all_libs.length).toStrictEqual(2)
   })
 })
