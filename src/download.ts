7252167991981992435980d7ee86dd22b3c25697
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-21 21:39:44 -0400

    Fix type errors

--- src/download.ts
+++ src/download.ts
@@ -7,20 +7,20 @@ import https from 'https'
 const body = async (response: EventEmitter): Promise<string> => await new Promise((resolve, reject) => {
   const buffer: string[] = []
   response
-    .on('data', (chunk: any) => buffer.push(chunk.toString()))
+    .on('data', (chunk: Buffer) => buffer.push(chunk.toString()))
     .on('error', reject)
     .on('end', () => resolve(buffer.join('')))
 })
 
 export async function download (url: string): Promise<string> {
-  let request: any
+  let request: unknown
   if (url.startsWith('https')) {
     request = https
   } else {
     request = http
   }
   return await new Promise((resolve, reject) => {
-    request
+    (request as typeof http)
       .get(url, {}, async (response: EventEmitter) => resolve(body(response)))
       .on('error', reject)
   })
