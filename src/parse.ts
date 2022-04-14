da4f60b698aeee75c8ea0072b2bb3e7c2d7a892c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-04-14 07:37:30 -0400

    Parse options

--- src/parse.ts
+++ src/parse.ts
@@ -1,8 +1,19 @@
 /// <reference lib="dom" />
 import { DOMParser } from '@xmldom/xmldom'
+import { getNamespaces, Library } from './ui5-cache'
 
-export async function parse (xml: string): Promise<Document> {
+export interface ParseOptions {
+  ui5?: {
+    cdn: string
+    version: string
+  }
+}
+
+export async function parse (xml: string, options?: ParseOptions): Promise<Document> {
+  const ui5Options: ParseOptions['ui5'] = options?.ui5 ?? { cdn: Library.openui5, version: 'latest' }
   const domParser = new DOMParser()
   const document = domParser.parseFromString(xml)
+  const namespaces = await getNamespaces(ui5Options.cdn, ui5Options.version)
+
   return document
 }
